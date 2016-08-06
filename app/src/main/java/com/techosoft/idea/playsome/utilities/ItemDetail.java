90246cba453f7f8608af936211239c680aefbb20
David <yiyashao@gmail.com>
2016-08-06 18:47:56 +0800

    last commit before load real data from cloud

--- app/src/main/java/com/techosoft/idea/playsome/utilities/ItemDetail.java
+++ app/src/main/java/com/techosoft/idea/playsome/utilities/ItemDetail.java
@@ -2,6 +2,7 @@ package com.techosoft.idea.playsome.utilities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -12,14 +13,18 @@ public class ItemDetail extends AppCompatActivity {
 
     TextView tvTitle, tvDetail, tvExpDate;
     Button btnGive, btnBack;
+    MyHelper myHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_detail);
 
+        //init helpers
+        myHelper = new MyHelper(this);
+
         //setup UI
-        tvTitle = (TextView)findViewById(R.id.tvItemDetail);
+        tvTitle = (TextView)findViewById(R.id.tvItemTitle);
         tvDetail = (TextView)findViewById(R.id.tvItemDetail);
         tvExpDate = (TextView)findViewById(R.id.tvItemExpDate);
         btnGive = (Button)findViewById(R.id.btnItemDetailGive);
@@ -29,6 +34,8 @@ public class ItemDetail extends AppCompatActivity {
         String title = this.getIntent().getExtras().getString("title");
         String detail = this.getIntent().getExtras().getString("detail");
         String expDate = this.getIntent().getExtras().getString("expDate");
+        Log.d(myHelper.mConst.LOG_TAG, "intent extracted: " + title + detail + expDate);
+
         tvTitle.setText(title);
         tvDetail.setText(detail);
         tvExpDate.setText(expDate);
